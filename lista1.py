9fb5c7b5abbdc64a4bf1cedce1d7930157c3ed05
Lucas Rocha dos Santos <lucasrocha77706@gmail.com>
2024-06-06 19:24:49 -0300

    Add files via upload

--- lista1.py
+++ lista1.py
@@ -8,28 +8,37 @@ G.add_nodes_from(range(1, 25))
 
 #coloque as arestas do seu grafo
 edges = [
-  (1, 12),
-  (2, 11), (2, 15),
-  (3, 19), (3, 14),
-  (4, 18), (4, 23),
-  (5, 13), (5, 20),
-  (6, 19), (6, 10),
-  (7, 8), (7, 16), (7, 12),
-  (8, 7), (8, 22), (8, 23),
+  (1, 12), 
+  (2, 11), (2, 15), 
+  (3, 19), (3, 14), 
+  (4, 18), (4, 23), 
+  (5, 13), (5, 20), 
+  (6, 19), (6, 10), 
+  (7, 8), (7, 16), (7, 12), 
+  (8, 22), (8, 23), 
   (9, 10), (9, 18), (9, 21),
-  (10, 24), (10, 14), 
-  (11, 15), (11, 14),
-  (12, 22),
-  (13, 20), (13, 24),
+  (10, 24), (10, 14),  
+  (11, 15), (11, 14), 
+  (12, 22), 
+  (13, 20), (13, 24), 
   (15, 20),
   (16, 17), (16, 21),
   (17, 20), (17, 19),
   (18, 23), (18, 21),
-  (20, 22),
+  (20, 22), 
   (23, 24)
 ]
 
 G.add_edges_from(edges)
+
+order = G.number_of_nodes()
+print(f"A ordem do grafo é: {order}")
+
+size = G.number_of_edges()
+print(f"O tamanho do grafo é: {size}")
+
+num_components = nx.number_connected_components(G)
+print(f"O número de componentes conexos é: {num_components}")
 
 pos = nx.circular_layout(G)
 nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='blue')
